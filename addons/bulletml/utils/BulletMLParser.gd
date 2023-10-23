7ccabe9df251ab8af8bed02bb477e7494db4e558
SolAZDev <SolAZDev@gmail.com>
2023-10-22 21:15:44 -0400

    Parser Draft 2 Complete

--- addons/bulletml/utils/BulletMLParser.gd
+++ addons/bulletml/utils/BulletMLParser.gd
@@ -8,9 +8,9 @@ static func ParseBML(file: String) -> BulletMLObject:
 	for child in data.children:
 		match BMLBaseType.ENodeName[child.name]:
 			BMLBaseType.ENodeName.action:
-				ParseAction(child, bml)
+				bml.action.append(ParseAction(child, bml))
 			BMLBaseType.ENodeName.fire:
-				ParseFire(child, bml)
+				bml.fire.append(ParseFire(child, bml))
 			BMLBaseType.ENodeName.bullet:
 				ParseBullet(child, bml)
 	return bml
@@ -42,50 +42,56 @@ static func ParseAction(
 		action.ref = name
 		action.type = BMLBaseType.ENodeName.actionRef
 	else:
-		match BMLBaseType.ENodeName[node.name]:
-			BMLBaseType.ENodeName.repeat:
-				action.type = BMLBaseType.ENodeName.repeat
-				action.ammount = ParseEquation(TryGetChildValue(node, BMLBaseType.ENodeName.times))
-			BMLBaseType.ENodeName.fire, BMLBaseType.ENodeName.fireRef:
-				action.type = BMLBaseType.ENodeName.fire
-				action.fire = ParseFire(node, bml)
-				pass
-			BMLBaseType.ENodeName.changeSpeed:
-				action.type = BMLBaseType.ENodeName.changeSpeed
-				action.ammount = TryGetChildValue(node, BMLBaseType.ENodeName.speed)
-				action.term = ParseEquation(TryGetChildValue(node, BMLBaseType.ENodeName.term))
-			BMLBaseType.ENodeName.changeDirection:
-				action.type = BMLBaseType.ENodeName.changeDirection
-				var dir = TryGetChildNode(node, BMLBaseType.ENodeName.direction)
-				if dir != null:
-					var type = TryGetAttribute(dir, "type")
-					action.direction_type = (
-						BMLBaseType.EDirectionType[type]
-						if type != null
-						else BMLBaseType.EDirectionType.absolute
-					)
-					action.direction = ParseEquation(dir.content)
-					action.term = ParseEquation(TryGetChildValue(node, BMLBaseType.ENodeName.term))
-			BMLBaseType.ENodeName.accel:
-				action.type = BMLBaseType.ENodeName.accel
-				# I honestly don't understand this bit just yet Isn't this what .direction is for?
-				var x = ParseEquation(TryGetChildValue(node, BMLBaseType.ENodeName.horizontal))
-				var y = ParseEquation(TryGetChildValue(node, BMLBaseType.ENodeName.vertical))
-				action.velocity = Vector2(x if x != null else 0, y if y != null else 0)
-				action.term = ParseEquation(TryGetChildValue(node, BMLBaseType.ENodeName.term))
-			BMLBaseType.ENodeName.wait:
-				action.type = BMLBaseType.ENodeName.wait
-				action.ammount = ParseEquation(node.content)
-			BMLBaseType.ENodeName.vanish:
-				action.type = BMLBaseType.ENodeName.vanish
-			BMLBaseType.ENodeName.action, BMLBaseType.ENodeName.actionRef:
-				action.type = BMLBaseType.ENodeName.action
-				ParseAction(node, bml, false, action)
+		for child in node.children:
+			match BMLBaseType.ENodeName[child.name]:
+				BMLBaseType.ENodeName.repeat:
+					action.type = BMLBaseType.ENodeName.repeat
+					var times = TryGetChildValue(child, BMLBaseType.ENodeName.times)
+					if times!=null: action.ammount = ParseEquation(times)
+				BMLBaseType.ENodeName.fire, BMLBaseType.ENodeName.fireRef:
+					action.type = BMLBaseType.ENodeName.fire
+					action.fire = ParseFire(child, bml)
+				BMLBaseType.ENodeName.changeSpeed:
+					action.type = BMLBaseType.ENodeName.changeSpeed
+					action.ammount = TryGetChildValue(child, BMLBaseType.ENodeName.speed)
+					var term = TryGetChildValue(child, BMLBaseType.ENodeName.term)
+					if term!=null: action.term = ParseEquation(term)
+				BMLBaseType.ENodeName.changeDirection:
+					action.type = BMLBaseType.ENodeName.changeDirection
+					var dir = TryGetChildNode(child, BMLBaseType.ENodeName.direction)
+					if dir != null:
+						var type = TryGetAttribute(dir, "type")
+						action.direction_type = (
+							BMLBaseType.EDirectionType[type]
+							if type != null
+							else BMLBaseType.EDirectionType.absolute
+						)
+						action.direction = ParseEquation(dir.content)
+						action.term = ParseEquation(TryGetChildValue(child, BMLBaseType.ENodeName.term))
+				BMLBaseType.ENodeName.accel:
+					action.type = BMLBaseType.ENodeName.accel
+					# I honestly don't understand this bit just yet Isn't this what .direction is for?
+					var dir = Vector2(0,0)
+					var x = TryGetChildValue(child, BMLBaseType.ENodeName.horizontal)
+					if x!=null: dir.x = ParseEquation(x)
+					var y = TryGetChildValue(child, BMLBaseType.ENodeName.vertical)
+					if y!=null: dir.y = ParseEquation(y)
+					action.velocity = dir
+					var term = TryGetChildValue(child, BMLBaseType.ENodeName.term)
+					if term!=null: action.term = ParseEquation(term)
+				BMLBaseType.ENodeName.wait:
+					action.type = BMLBaseType.ENodeName.wait
+					action.ammount = ParseEquation(node.content)
+				BMLBaseType.ENodeName.vanish:
+					action.type = BMLBaseType.ENodeName.vanish
+				BMLBaseType.ENodeName.action, BMLBaseType.ENodeName.actionRef:
+					action.type = BMLBaseType.ENodeName.action
+					ParseAction(child, bml, false, action)
+		action.label = name
 		if parent_action != null:
 			parent_action.actions.append(action)
 		if refable:
 			# Reset and Return Reference
-			action.label = name
 			bml.action.append(action)
 			action = BulletMLAction.new()
 			action.ref = name
@@ -111,7 +117,8 @@ static func ParseFire(node: XMLNode, bml: BulletMLObject) -> BulletMLFire:
 				else BMLBaseType.EDirectionType.absolute
 			)
 			fire.direction = ParseEquation(dir.content)
-		fire.speed = TryGetChildValue(node, BMLBaseType.ENodeName.speed)
+		var speed = TryGetChildValue(node, BMLBaseType.ENodeName.speed)
+		if speed !=null: fire.speed = speed
 		var bullet = TryGetChildNode(node, BMLBaseType.ENodeName.bullet)
 		if bullet != null:
 			fire.bullet = ParseBullet(bullet, bml)
@@ -135,12 +142,15 @@ static func ParseBullet(node: XMLNode, bml: BulletMLObject) -> BMLBullet:
 		bullet.label = (
 			node.attributes.get("label") if node.attributes.has("label") else str(bml.bullet.size())
 		)
-		bullet.speed = TryGetChildValue(node, BMLBaseType.ENodeName.speed)
-		bullet.damage = TryGetAttribute(node, "damage")
-		bullet.lifetime = TryGetAttribute(node, "damage")
+		var speed = TryGetChildValue(node, BMLBaseType.ENodeName.speed)
+		if speed!=null: bullet.speed = speed
+		var damage = TryGetAttribute(node, "damage")
+		if damage!=null: bullet.damage = damage
+		var lifetime = TryGetAttribute(node, "lifetime")
+		if lifetime!=null: bullet.lifetime = lifetime
 		var action = TryGetChildNode(node, BMLBaseType.ENodeName.action)
 		if action != null:
-			bullet.action = ParseAction(node, bml, true)
+			bullet.action = ParseAction(action, bml, true)
 		bml.bullet.append(bullet)
 		var ref_name = bullet.label
 		# Reset and Return Reference
@@ -158,7 +168,9 @@ static func TryGetAttribute(node: XMLNode, key: String) -> Variant:
 
 static func TryGetChildValue(node: XMLNode, type: BMLBaseType.ENodeName) -> Variant:
 	var possible_children = node.children.filter(
-		func(x: XMLNode): return BMLBaseType.ENodeName[x.name] == type
+		func(x: XMLNode):
+			var atype = BMLBaseType.ENodeName[x.name] 
+			return atype == type
 	)
 	if possible_children.size() > 0:
 		return possible_children[0].content
@@ -168,11 +180,8 @@ static func TryGetChildValue(node: XMLNode, type: BMLBaseType.ENodeName) -> Vari
 
 static func TryGetChildNode(node: XMLNode, type: BMLBaseType.ENodeName) -> XMLNode:
 	var possible_children = (
-		node
-		. children
-		. filter(
-			func(x: XMLNode): return x.name.contains(str(type))
-			# return BMLBaseType.ENodeName[x.name] == type
+		node.children.filter(
+			 func (x:XMLNode): return BMLBaseType.ENodeName[x.name] == type
 		)
 	)
 	if possible_children.size() > 0:
@@ -182,4 +191,13 @@ static func TryGetChildNode(node: XMLNode, type: BMLBaseType.ENodeName) -> XMLNo
 
 
 static func ParseEquation(equation: String) -> float:
-	return 0
+	var exp = Expression.new()
+	var result = 0
+	var finalString = equation.replace("$rank", "1").replace("$rand", str(randf_range(0,1)))
+	var err = exp.parse(finalString)
+	if err == OK: result = exp.execute()
+	return result
+
+static func NameToType(name:String) -> BMLBaseType.ENodeName:
+	var type =  BMLBaseType.ENodeName.keys()[BMLBaseType.ENodeName[name]]
+	return type
--- addons/bulletml/xml_data/BulletMLObject.gd
+++ addons/bulletml/xml_data/BulletMLObject.gd
@@ -1,6 +1,6 @@
 class_name BulletMLObject
 
 var action:Array[BulletMLAction]
-var bullet:Array[BMLBullet]
 var fire:Array[BulletMLFire]
+var bullet:Array[BMLBullet]
  
